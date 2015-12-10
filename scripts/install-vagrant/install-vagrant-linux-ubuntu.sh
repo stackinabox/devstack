183aeefaf8ac09ae611a0eb37883e6c71a4b5043
Tim Pouyer <tpouyer@us.ibm.com>
2015-12-10 08:52:37 -0500

    Updates for travis-ci automated builds

--- scripts/install-vagrant/install-vagrant-linux-ubuntu.sh
+++ scripts/install-vagrant/install-vagrant-linux-ubuntu.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
 sudo sh -c "echo 'deb http://download.virtualbox.org/virtualbox/debian '$(lsb_release -cs)' contrib non-free' > /etc/apt/sources.list.d/virtualbox.list" 
 wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add -
