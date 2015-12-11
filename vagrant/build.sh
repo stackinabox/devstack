113e296a971d5b944a59371425aaa75fe778802d
Tim Pouyer <tpouyer@us.ibm.com>
2015-12-10 22:14:45 -0500

    reduce size of vagrant vm to fit travis-ci environment

--- vagrant/build.sh
+++ vagrant/build.sh
@@ -2,8 +2,21 @@
 
 topDir=$(pwd)
 pushd $topDir/vagrant
-cp $topDir/vagrant/Personalization.dist $topDir/vagrant/Personalization
 cat >> $topDir/vagrant/Personalization <<EOF
+# Use NFS? (won't work on windows)
+\$use_nfs = false
+
+# Box name
+\$box = "stackinabox"
+
+# Box url
+\$box_url = "$(topDir)/build/stackinabox.box"
+
+# Number of CPU's (min 2, recommend 4) adjust to your machine
+\$cpus = 2
+
+# Amount of RAM (min 4096, recommed 8192) adjust to your machine
+\$memory = 4096
 
 # Second Disk (used for lxd backing store)
 \$disk = '$HOME/VirtualBox VMs/stackinabox/box-disk2.vmdk'
