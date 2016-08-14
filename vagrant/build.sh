b108125376776a81ded513c469ab07dcf028cc80
sudhakarau1 <sudhakarau1@users.noreply.github.com>
2016-08-15 09:36:59 +1000

    Mitaka and 16.04 update

--- vagrant/build.sh
+++ vagrant/build.sh
@@ -4,26 +4,31 @@ topDir=$(pwd)
 pushd $topDir/vagrant
 
 touch $topDir/vagrant/Personalization
-cat >> $topDir/vagrant/Personalization <<EOF
+cat > $topDir/vagrant/Personalization <<EOF
 # Use NFS? (won't work on windows)
 \$use_nfs = false
 
 # Box name
-\$box = "ubuntu/trusty64"
+\$box = "bento/ubuntu-16.04"
 
 # Box url
-\$box_url = "https://atlas.hashicorp.com/ubuntu/boxes/trusty64"
+\$box_url = "https://atlas.hashicorp.com/bento/boxes/ubuntu-16.04"
 
 # Number of CPU's (min 2, recommend 4) adjust to your machine
-\$cpus = 4
+\$cpus = 8
 
 # Amount of RAM (min 4096, recommed 8192) adjust to your machine
-\$memory = 8192
+\$memory = 16384
+
+# Which release branch should we build? ( stable/juno | stable/kilo | stable/liberty | master )
+\$release_branch = "stable/mitaka"
 
 # Second Disk (used for lxd backing store)
 \$disk = '$HOME/VirtualBox VMs/stackinabox/box-disk2.vmdk'
 EOF
-vagrant up --provider=virtualbox
+cat $topDir/vagrant/Personalization
+vagrant up --provision-with "openstack" --provider=virtualbox
+vagrant reload
+vagrant provision --provision-with "post-config"
 sleep 60 #wait for OpenStack services in vm to start completely
 popd
-
